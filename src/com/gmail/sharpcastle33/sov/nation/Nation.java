230e1c180eed093be532aa949b29a188585124ac
Stephen/Sharpcastle33 <sharpcastle33@gmail.com>
2015-12-16 15:13:08 -0500

    .

--- src/com/gmail/sharpcastle33/sov/nation/Nation.java
+++ src/com/gmail/sharpcastle33/sov/nation/Nation.java
@@ -14,6 +14,7 @@ public class Nation {
 ArrayList<UUID> members;
 ArrayList<UUID> leaders;
 ArrayList<Sovereignty> sov;
+String name;
 		
 //+============+
 //|CONSTRUCTION|
@@ -30,5 +31,42 @@ public Nation(){
 //+=======+
 //|GET/SET|
 //+=======+
+
+	public ArrayList<UUID> getMembers(){
+		return this.members;
+	}
+	
+	public ArrayList<UUID> getLeaders(){
+		return this.leaders;
+	}
+	
+	public void setMembers(ArrayList<UUID> l){
+		this.members = l;
+	}
+	
+	public void addMember(UUID u){
+		this.members.add(u);
+	}
+	
+	public void addLeader(UUID u){
+		this.leaders.add(u);
+	}
+	
+	public void removeMember(UUID u){
+		if(this.members.contains(u)){
+			this.members.remove(u);
+		}
+	}
+	
+	public void removeLeader(UUID u){
+		if(this.leaders.contains(u)){
+			this.leaders.remove(u);
+		}
+	}
+	
+	public void setLeaders(ArrayList<UUID> l){
+		this.leaders = l;
+	}
+
 
 }
